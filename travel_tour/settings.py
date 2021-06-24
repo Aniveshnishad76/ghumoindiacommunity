1f9bc98df1cf814e6a2f1d3d5ac7c02dd675f9ad
Anivesh nishad <78677890+Aniveshnishad76@users.noreply.github.com>
2021-06-24 12:42:59 +0530

    Update settings.py

--- travel_tour/settings.py
+++ travel_tour/settings.py
@@ -145,8 +145,8 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
-EMAIL_HOST_USER="anivesh.nishad07@gmail.com"
-EMAIL_HOST_PASSWORD="Nishad@1234"
+EMAIL_HOST_USER="justforhost26@gmail.com"
+EMAIL_HOST_PASSWORD="Modi3008@"
 EMAIL_USE_SSL=False
 
 
