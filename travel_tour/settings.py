a3356c5b8d3610cd998e3313bd066dfcaba0a3c5
Anivesh nishad <78677890+Aniveshnishad76@users.noreply.github.com>
2021-06-29 19:00:12 +0530

    Update settings.py

--- travel_tour/settings.py
+++ travel_tour/settings.py
@@ -141,12 +141,14 @@ STATICFILES_DIRS=(
 
 
 )
-EMAIL_HOST="mail.freekamall.tech"
-EMAIL_PORT=465
-EMAIL_USE_TLS=False
-EMAIL_HOST_USER="investit@freekamall.tech"
-EMAIL_HOST_PASSWORD="s@S7223889629"
-EMAIL_USE_SSL=True
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
+EMAIL_HOST_USER="anivesh.nishad07@gmail.com"
+EMAIL_HOST_PASSWORD="Nishad@1234"
+EMAIL_USE_SSL=False
+
 
 
 MESSAGE_TAGS ={
